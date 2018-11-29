c6ddea089c1620763e2529b0aaf18f58166d231c
glenndomin <glenn.domin@ose.com.au>
2018-11-30 09:53:35 +1000

    amending button class conditions

--- src/vue-datatable-pager-button.vue
+++ src/vue-datatable-pager-button.vue
@@ -2,7 +2,7 @@
 
 <template>
 	<li :class="li_classes">
-		<a href="javascript: void(0);" :class="[a_classes, 'px-3 px-md-4']" @click="sendClick">
+		<a href="javascript: void(0);" :class="[a_classes, value === 'Prev' || value === 'Next' ? 'px-3 px-md-4' : '']" @click="sendClick">
 			<slot>{{ value }}</slot>
 		</a>
 	</li>
