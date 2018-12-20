621c3b117b9865e1a0047347af32b240703fe175
glenn-domin <glenn.domin@ose.com.au>
2018-12-20 14:23:23 +1000

    fixing v-html directive for pager buttons

--- src/vue-datatable-pager-button.vue
+++ src/vue-datatable-pager-button.vue
@@ -3,7 +3,7 @@
 <template>
 	<li :class="li_classes">
 		<a href="javascript: void(0);" :class="[a_classes, prevNext === true ? 'px-3 px-md-4' : '']" @click="sendClick">
-			<slot v-html="rawHtml">{{ value }}</slot>
+			<slot v-html="value"></slot>
 		</a>
 	</li>
 </template>
